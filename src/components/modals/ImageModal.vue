e08b6af3bc5c2d4454d6505b65d55f41609b66c5
CodingYourLife <10122382+codingyourlife@users.noreply.github.com>
2024-02-07 07:26:44 +0100

    handle google drive image link resolution

--- src/components/modals/ImageModal.vue
+++ src/components/modals/ImageModal.vue
@@ -5,15 +5,18 @@
       <form-entry label="URL" error="url">
         <input slot="field" class="textfield" type="text" v-model.trim="url" @keydown.enter="resolve">
       </form-entry>
-      <menu-entry @click.native="openGooglePhotos(token)" v-for="token in googlePhotosTokens" :key="token.sub">
+      <!-- <menu-entry @click.native="openGooglePhotos(token)" v-for="token in googlePhotosTokens" :key="token.sub">
         <icon-provider slot="icon" provider-id="googlePhotos"></icon-provider>
-        <div>Open from Google Photoz</div>
+        <div>Open from Google Photos</div>
         <span>{{token.name}}</span>
       </menu-entry>
       <menu-entry @click.native="addGooglePhotosAccount">
         <icon-provider slot="icon" provider-id="googlePhotos"></icon-provider>
         <span>Add Google Photos account</span>
-      </menu-entry>
+      </menu-entry> -->
+      <p>
+        <small>Note: You can also paste public share links of Google Drive</small>
+      </p>
     </div>
     <div class="modal__button-bar">
       <button class="button" @click="reject()">Cancel</button>
@@ -27,6 +30,32 @@ import modalTemplate from './common/modalTemplate';
 import MenuEntry from '../menus/common/MenuEntry';
 import googleHelper from '../../services/providers/helpers/googleHelper';
 import store from '../../store';
+
+function resolveGoogleDriveUrl(url) {
+  if (!url) {
+    console.error('URL is required');
+    return url;
+  }
+
+  const googleDrivePattern = /https:\/\/drive\.google\.com\/file\/d\/([a-zA-Z0-9_-]+)\/view\?usp=sharing/;
+  const match = url.match(googleDrivePattern);
+
+  if (match) {
+    const transformedUrl = `https://lh3.google.com/u/0/d/${match[1]}?export=view&authuser=0`;
+
+    // Show a confirm dialog to the user
+    const confirmMessage = "A new tab will open where you can copy the URL. Are you sure you want to proceed?";
+    if (window.confirm(confirmMessage)) {
+      // Open the transformed URL in a new tab if the user confirms
+      window.open(transformedUrl, '_blank');
+      return "";
+    }
+  } else {
+    console.debug('Provided URL does not match the expected Google Drive URL pattern.');
+  }
+
+  return url;
+}
 
 export default modalTemplate({
   components: {
@@ -49,9 +78,14 @@ export default modalTemplate({
       if (!this.url) {
         this.setError('url');
       } else {
+        this.url = resolveGoogleDriveUrl(this.url);
+
         const { callback } = this.config;
         this.config.resolve();
-        callback(this.url);
+
+        if(this.url) {
+          callback(this.url);
+        }
       }
     },
     reject() {
